da4f2123cd3d5309e8003c7ffae1accc2821a5fc
Moritz Schilling <moritz.schilling@dfki.de>
2018-09-19 13:20:20 +0100

    Improved button handling
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- joystick_2_lights/joystick_2_lights.cc
+++ joystick_2_lights/joystick_2_lights.cc
@@ -5,7 +5,9 @@
 #include "Context-joystick-2-lights.h"
 #include <iostream>
 
+static bool white_lights_already_switched = false;
 static bool white_lights_on = false;
+static bool uv_lights_already_switched = false;
 static bool uv_lights_on = false;
 
 void joystick_2_lights_startup()
@@ -23,8 +25,16 @@ void joystick_2_lights_PI_commands(const asn1SccJoystickCommand *IN_cmd)
     {
         std::cout << "[joystick_2_lights_PI_commands] Button for white lights does not exist\n";
     } else {
-        if ( IN_cmd->buttons.elements.arr[joystick_2_lights_ctxt.white_light_button] > 0)
-            white_lights_on = !white_lights_on;
+        if ( (IN_cmd->buttons.elements.arr[joystick_2_lights_ctxt.white_light_button] > 0) )
+        {
+            if (!white_lights_already_switched)
+            {
+                white_lights_on = !white_lights_on;
+            }
+            white_lights_already_switched = true;
+        } else {
+            white_lights_already_switched = false;
+        }
         const asn1SccT_Boolean on(white_lights_on);
         joystick_2_lights_RI_white_lights(&on);
     }
@@ -33,7 +43,15 @@ void joystick_2_lights_PI_commands(const asn1SccJoystickCommand *IN_cmd)
         std::cout << "[joystick_2_lights_PI_commands] Button for uv lights does not exist\n";
     } else {
         if ( IN_cmd->buttons.elements.arr[joystick_2_lights_ctxt.uv_light_button] > 0)
-            uv_lights_on = !uv_lights_on;
+        {
+            if (!uv_lights_already_switched)
+            {
+                uv_lights_on = !uv_lights_on;
+            }
+            uv_lights_already_switched = true;
+        } else {
+            uv_lights_already_switched = false;
+        }
         const asn1SccT_Boolean on(uv_lights_on);
         joystick_2_lights_RI_uv_lights(&on);
     }
